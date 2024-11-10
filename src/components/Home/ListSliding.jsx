12531f5c11a47d33a7de847168a74c165fbae07e
Pradheep Raj <pradheepselva111@gmail.com>
2024-11-10 16:26:41 +0530

    added home slider animation

--- src/components/Home/ListSliding.jsx
+++ src/components/Home/ListSliding.jsx
@@ -1,10 +1,60 @@
 import React from "react";
 import { list1 } from "../../Data/HomePageInfo";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import gsap from "gsap";
 
 const ListSliding = () => {
+  const list1Ref = useRef(null)
+  const list2Ref = useRef(null)
+  const list3Ref = useRef(null)
+  const t1 = gsap.timeline();
+  const t2 = gsap.timeline();
+  const t3 = gsap.timeline();
+
+  useGSAP(() => {
+    console.log(list1Ref.current?.scrollWidth)
+    if(list1Ref.current){
+      t1.to(list1Ref.current,{
+        x : -list1Ref.current.scrollWidth/2-10, //10 was subtracted because we given a gap of 20px each so the two sets will also have that gap
+        duration : 30,
+        ease : "none",
+        repeat : -1
+      })
+      // gsap.set(list1Ref.current,{
+      //     x : -list1Ref.current.scrollWidth/2-10,
+      // })
+    }
+      if(list2Ref.current){
+        t2.from(list2Ref.current,{
+          x : -list2Ref.current.scrollWidth/2-10,
+          duration : 30,
+          ease : "none",
+          repeat : -1
+        })
+      }  
+      if(list3Ref.current){
+      t3.to(list3Ref.current,{
+        x : -list3Ref.current.scrollWidth/2-10,
+        duration : 30,
+        ease : "none",
+        repeat : -1
+      })
+    }
+  },[])
   return (
     <div className="flex flex-col gap-10 py-32 overflow-x-scroll no-scrollbar">
-      <div className="flex gap-5">
+      <div className="flex gap-5" ref={list1Ref} onMouseOver={() => t1.pause()} onMouseLeave={() => t1.play()}>
+        {list1.map((ele, index) => {
+          return (
+            <div
+              className="bg-[#171717] min-w-[400px] p-7 rounded-xl"
+              key={index}
+            >
+              <h1>{ele} &#8599;</h1>
+            </div>
+          );
+        })}
         {list1.map((ele, index) => {
           return (
             <div
@@ -16,7 +66,17 @@ const ListSliding = () => {
           );
         })}
       </div>
-      <div className="flex gap-5 -ml-[200px]">
+      <div className="flex gap-5 -ml-[200px]" ref={list2Ref} onMouseOver={() => t2.pause()} onMouseLeave={() => t2.play()}>
+        {list1.map((ele, index) => {
+          return (
+            <div
+              className="bg-[#171717] min-w-[400px] p-7 rounded-xl"
+              key={index}
+            >
+              <h1>{ele} &#8599;</h1>
+            </div>
+          );
+        })}
         {list1.map((ele, index) => {
           return (
             <div
@@ -28,7 +88,17 @@ const ListSliding = () => {
           );
         })}
       </div>
-      <div className="flex gap-5">
+      <div className="flex gap-5" ref={list3Ref} onMouseOver={() => t3.pause()} onMouseLeave={() => t3.play()}>
+        {list1.map((ele, index) => {
+          return (
+            <div
+              className="bg-[#171717] min-w-[400px] p-7 rounded-xl"
+              key={index}
+            >
+              <h1>{ele} &#8599;</h1>
+            </div>
+          );
+        })}
         {list1.map((ele, index) => {
           return (
             <div
